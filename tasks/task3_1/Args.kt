158cf1c5727d66241b03e4d34a52622a5db32a14
Vedant <nrtc1373@leeds.ac.uk>
2025-10-06 09:27:38 +0000

    week3

--- tasks/task3_1/Args.kt
+++ tasks/task3_1/Args.kt
@@ -1,1 +1,10 @@
+import kotlin.system.exitProcess
+
+fun main(args: Array<String>) {
+	if (args.size < 2) {
+		exitProcess(1)
+	}
+	println(args[0])
+	println(args[1])
+}
 // Task 3.1: command line arguments
---
+++ tasks/task3_2/Read.kt
@@ -0,0 +1,7 @@
+import kotlin.system.exitProcess
+
+fun main(args: Array<String>) {
+    print("enter your name: ")
+    val name = readln()
+    println(name.length)
+}
